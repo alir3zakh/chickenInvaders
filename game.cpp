9d16afa6b0f63758be53ecd1c696252144557e99
alireza-kherad <alirezakherad35@gmail.com>
2016-06-23 02:03:57 +0430

    windows size bug fixed

--- game.cpp
+++ game.cpp
@@ -3,13 +3,14 @@
 
 Game::Game(QWidget * parent) : QGraphicsView(parent) {
     lvl = new Level1();
-    setStyleSheet("background-image: url(:/img/Bground.jpg); background-position: 0px 0px;");
+    setStyleSheet("background-image: url(:/img/Bground.jpg); background-position: 0px 0px;"
+                  "background-repeat: repeat-x; background-attachment: scroll");
 
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    //setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     setScene(lvl);
-    this->setFixedSize(1250, 700);
+    this->setFixedSize(1200, 700);
 
     this->show();
 
